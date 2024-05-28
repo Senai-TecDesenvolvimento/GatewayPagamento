41031d3406670665f1778c5a75b826ee5daa9bc4
Rinchuu <thiago_tietjen@estudante.sesisenai.org.br>
2024-05-28 20:22:09 -0300

    Implementado EP GET para boleto
    funções para cadastro de pagamento implementadas em BoletoService e BoletoController criadas com o metodo RequestMethod.GET

--- src/main/java/com/tdesi/senai/pagamento/service/BoletoService.java
+++ src/main/java/com/tdesi/senai/pagamento/service/BoletoService.java
@@ -1,7 +1,17 @@
 package com.tdesi.senai.pagamento.service;
 
+import com.tdesi.senai.pagamento.entity.Boleto;
+import com.tdesi.senai.pagamento.repository.BoletoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoletoService {
+
+    @Autowired
+    private BoletoRepository boletoRepository;
+
+    public Boleto cadastrarBoleto(Boleto boleto) {
+        return boletoRepository.save(boleto);
+    }
 }
