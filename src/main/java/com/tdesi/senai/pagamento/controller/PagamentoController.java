dbee61c172c4155542101c3e8a823310c6160399
Willian <nkboita@gmail.com>
2024-05-28 19:35:09 -0300

    Método POST adicionado a pagamentos

--- src/main/java/com/tdesi/senai/pagamento/controller/PagamentoController.java
+++ src/main/java/com/tdesi/senai/pagamento/controller/PagamentoController.java
@@ -1,19 +1,31 @@
 package com.tdesi.senai.pagamento.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tdesi.senai.pagamento.entity.Pagamento;
+import com.tdesi.senai.pagamento.service.PagamentoService;
 
 
 @RestController
 @RequestMapping(value = "/pagamento")
 public class PagamentoController {
+	
+	@Autowired
+	private PagamentoService service;
 		
 	@RequestMapping(method = RequestMethod.GET, value="teste")
-    public ResponseEntity<String> teste() {
+    public ResponseEntity<String> pagamento() {
         return ResponseEntity.ok("OK");
     }
 	
+	@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity insert (@RequestBody Pagamento pagamento) {
+        return ResponseEntity.ok(service.insert(pagamento));
+    }
 	
 }
