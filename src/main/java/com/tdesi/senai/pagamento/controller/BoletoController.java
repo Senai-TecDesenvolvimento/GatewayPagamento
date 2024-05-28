f6c1eacb7c3c884457450ac50ecea0c284e12c99
joabe <joabe.silva@softplan.com.br>
2024-05-28 19:43:11 -0300

    meu commit do cadastro de Boleto

--- src/main/java/com/tdesi/senai/pagamento/controller/BoletoController.java
+++ src/main/java/com/tdesi/senai/pagamento/controller/BoletoController.java
@@ -1,6 +1,9 @@
 package com.tdesi.senai.pagamento.controller;
 
+import com.tdesi.senai.pagamento.service.BoletoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/boleto")
 public class BoletoController {
+
+    @Autowired
+    private BoletoService service;
 		
 	@RequestMapping(method = RequestMethod.GET, value="teste")
     public ResponseEntity<String> teste() {
         return ResponseEntity.ok("OK");
     }
-	
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity insert (@RequestBody Boleto boleto) {
+        return ResponseEntity.ok(service.insert(boleto));
+    }
 	
 }
--- src/main/java/com/tdesi/senai/pagamento/service/BoletoService.java
+++ src/main/java/com/tdesi/senai/pagamento/service/BoletoService.java
@@ -1,7 +1,27 @@
 package com.tdesi.senai.pagamento.service;
 
+import com.tdesi.senai.pagamento.entity.Cartao;
+import com.tdesi.senai.pagamento.entity.Teste;
+import com.tdesi.senai.pagamento.repository.BoletoRepository;
+import com.tdesi.senai.pagamento.repository.CartãoRepository;
+import com.tdesi.senai.pagamento.repository.TesteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoletoService {
+
+    @Autowired
+    private BoletoRepository repository;
+
+    public String teste() {
+        return "OK";
+    }
+
+    public Boleto insert(Boleto boleto) {
+        Boleto t = repository.save(boleto);
+        return t;
+    }
 }
