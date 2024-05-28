af54ea59dcb023a237c5a9850174e101f1467ec0
lArauj0 <macregisjosue@gmail.com>
2024-05-27 22:16:58 -0300

    Implementação do Repositório JPA para Cartão

--- src/main/java/com/tdesi/senai/pagamento/repository/CartaoRepository.java
+++ src/main/java/com/tdesi/senai/pagamento/repository/CartaoRepository.java
@@ -1,5 +1,9 @@
 package com.tdesi.senai.pagamento.repository;
 
-public interface CartaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tdesi.senai.pagamento.entity.Pagamento;
+
+public interface CartaoRepository extends JpaRepository <Pagamento, Long> {
 
 }
