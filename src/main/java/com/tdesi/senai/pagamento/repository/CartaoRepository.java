c42ce8e1d983b689da3692d262b8ed2bde021086
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2024-05-28 19:03:11 -0300

    removido classe repitida

--- src/main/java/com/tdesi/senai/pagamento/repository/CartaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tdesi.senai.pagamento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tdesi.senai.pagamento.entity.Pagamento;
-
-public interface CartaoRepository extends JpaRepository <Pagamento, Long> {
-
-}
