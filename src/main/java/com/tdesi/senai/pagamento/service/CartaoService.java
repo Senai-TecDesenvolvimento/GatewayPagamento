0f77a7da2ef73904869ffa503520a02fa358d06c
lArauj0 <macregisjosue@gmail.com>
2024-05-28 19:28:11 -0300

    Criar EP para cadastro de cartão

--- src/main/java/com/tdesi/senai/pagamento/service/CartaoService.java
+++ src/main/java/com/tdesi/senai/pagamento/service/CartaoService.java
@@ -1,8 +1,24 @@
 package com.tdesi.senai.pagamento.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tdesi.senai.pagamento.entity.Cartao;
+import com.tdesi.senai.pagamento.repository.CartãoRepository;
+
 
 @Service
 public class CartaoService {
 
+	@Autowired
+	private CartãoRepository repository;
+	
+	public String teste() {
+		return "OK";
+	}
+
+	public Cartao insert(Cartao cartao) {
+		Cartao t = repository.save(cartao);
+		return t;
+	}
 }
